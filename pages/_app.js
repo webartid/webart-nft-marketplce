9647650a7488c47d69554de6870a65e59f845b68
ichone <ichone@users.noreply.github.com>
2022-08-02 19:19:27 +0700

    finishing v0

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { NFTProvider } from '../context/NFTContext';
+import { Footer, Navbar } from '../components';
+import '../styles/globals.css';
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => (
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
+
+      <Script src="https://kit.fontawesome.com/b2860fcb96.js" crossorigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
+
+);
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,189 @@
-const Home = () => (
-  <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello World
-    </h1>?
-  </div>
-);
+import { useState, useEffect, useRef, useContext } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+import { getCreators } from '../utils/getTopCreators';
+
+import { Banner, CreatorCard, Loader, NFTCard, SearchBar } from '../components';
+
+import { NFTContext } from '../context/NFTContext';
+import images from '../assets';
+import { makeId } from '../utils/makeId';
+import { shortenAddress } from '../utils/shortenAddress';
+
+const Home = () => {
+  const { fetchNFTs } = useContext(NFTContext);
+  const [hideButtons, sethideButtons] = useState(false);
+  const [nfts, setNfts] = useState([]);
+  const [nftsCopy, setNftsCopy] = useState([]);
+  const parentRef = useRef(null);
+  const scrollRef = useRef(null);
+  const { theme } = useTheme();
+  const [activeSelect, setActiveSelect] = useState('Recently Added');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetchNFTs()
+      .then((items) => {
+        setNfts(items);
+        setNftsCopy(items);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    const sortedNfts = [...nfts];
+
+    switch (activeSelect) {
+      case 'Price (low to high)':
+        setNfts(sortedNfts.sort((a, b) => a.price - b.price));
+        break;
+      case 'Price (high to low)':
+        setNfts(sortedNfts.sort((a, b) => b.price - a.price));
+        break;
+      case 'Recently added':
+        setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
+        break;
+      default:
+        setNfts(nfts);
+        break;
+    }
+  }, [activeSelect]);
+
+  const onHandleSearch = (value) => {
+    const filteredNfts = nfts.filter(({ name }) => name.toLowerCase().includes(value.toLowerCase()));
+
+    if (filteredNfts.length) {
+      setNfts(filteredNfts);
+    } else {
+      setNfts(nftsCopy);
+    }
+  };
+
+  const onClearSearch = () => {
+    if (nfts.length && nftsCopy.length) {
+      setNfts(nftsCopy);
+    }
+  };
+
+  const handleScroll = (direction) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollWidth >= parent?.offsetWidth) {
+      sethideButtons(false);
+    } else {
+      sethideButtons(true);
+    }
+  };
+
+  useEffect(() => {
+    isScrollable();
+    window.addEventListener('resize', isScrollable);
+
+    return () => {
+      window.removeEventListener('resize', isScrollable);
+    };
+  });
+
+  const topCreators = getCreators(nftsCopy);
+
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name={(<>Discover, collect, and sell<br />extraordinary NFTs</>)}
+          childStyles="md:text-4xl sm:text-2xl xs=text-xl text-left"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />
+        {!isLoading && !nfts.length ? (
+          <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">That&apos;s weird... No NFTs for sale!</h1>
+        ) : isLoading ? <Loader /> : (
+          <>
+            <div>
+              <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Top Sellers</h1>
+              <div
+                className="relative flex-1 max-w-full flex mt-3"
+                ref={parentRef}
+              >
+                <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none" ref={scrollRef}>
+                  {topCreators.map((creator, i) => (
+                    <CreatorCard
+                      key={creator.seller}
+                      rank={i + 1}
+                      creatorImage={images[`creator${i + 1}`]}
+                      creatorName={shortenAddress(creator.seller)}
+                      creatorEths={creator.sum}
+                    />
+                  ))}
+                  {/* {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i}`]}
+                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
+                  creatorEths={10 - i * 0.5}
+                />
+              ))} */}
+                  {!hideButtons && (
+                  <>
+                    <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                      <Image src={images.left} layout="fill" objectFit="contain" alt="left" className={theme === 'light' ? 'filter invert' : ''} />
+                    </div>
+                    <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                      <Image src={images.right} layout="fill" objectFit="contain" alt="left" className={theme === 'light' ? 'filter invert' : ''} />
+                    </div>
+                  </>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="mt-10">
+              <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+                <h1 className="flex-1 nefore:first:font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot NFTs</h1>
+                <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
+                  <SearchBar
+                    activeSelect={activeSelect}
+                    setActiveSelect={setActiveSelect}
+                    handleSearch={onHandleSearch}
+                    clearSearch={onClearSearch}
+                  />
+                </div>
+              </div>
+              <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+                {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)}
+                {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  desription: 'Cool NFT on Sale',
+                }}
+              />
+            ))} */}
+              </div>
+            </div>
+          </>
+        )}
+
+      </div>
+    </div>
+  );
+};
 
 export default Home;
